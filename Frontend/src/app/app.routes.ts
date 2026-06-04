658e2f09e79df07a91dbc69eff9175db51a66c45
Tempish <92348966+Tempish@users.noreply.github.com>
2026-06-03 21:21:27 -0600

    Arreglos al espacio de trabajo
    Creación de home (index) y creación de rutas a otros módulos

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,17 +1,35 @@
 import { Routes } from '@angular/router';
+
+import { HomePage } from './pages/home/home-page';
 import { ClientesPage } from './pages/clientes/clientes-page';
+import { ProductosPage } from './pages/productos/productos-page';
+import { FacturasPage } from './pages/facturas/facturas-page';
 
 export const routes: Routes = [
+
   {
     path: '',
+    component: HomePage
+  },
+
+  {
+    path: 'clientes',
     component: ClientesPage
   },
+
   {
     path: 'productos',
-    component: ClientesPage
+    component: ProductosPage
   },
+
   {
     path: 'facturas',
-    component: ClientesPage
+    component: FacturasPage
+  },
+
+  {
+    path: '**',
+    redirectTo: ''
   }
+
 ];
---
+++ Frontend/src/app/pages/facturas/facturas-page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-facturas-page',
+  template: `
+    <h2>Facturas</h2>
+    <p>Módulo en desarrollo.</p>
+  `
+})
+export class FacturasPage {}
